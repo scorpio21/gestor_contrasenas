c93b06f0c7fa708c31114bc4ac2c9bb62a25b0eb
scorpio21 <sonscorpio@gmail.com>
2025-08-31 02:52:23 +0200

    CI: dotnet-format y tests; agrega proyecto xUnit base

---
+++ test/GestorContrasenas.Tests/SampleTests.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace GestorContrasenas.Tests
+{
+    public class PruebasBasicas
+    {
+        [Fact]
+        public void Debe_pasarse_verdadero()
+        {
+            // Prueba mínima para validar el pipeline de CI
+            Assert.True(true);
+        }
+    }
+}
